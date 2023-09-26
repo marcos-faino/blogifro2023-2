beb44ad589cb3bd9c88ec0953686aead47ebaf2f
Marcos Alves Faino <1812293@ifro.local>
2023-09-25 22:19:51 -0400

    tentando enviar email

--- blogifro2023/settings.py
+++ blogifro2023/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
 
     'postagens',
 ]
@@ -126,3 +127,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# email para testes
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- postagens/urls.py
+++ postagens/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('listar/', views.PostsListView.as_view(), name='listarposts'),
     path('detalhe/<id>/<slug>', views.DetalhePostView.as_view(), name='detalhepost'),
+    path('enviar/<int:pk>/', views.EnviarPostFormView.as_view(), name='enviarpost'),
 ]
